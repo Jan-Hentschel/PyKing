b62a8a77c5e87cc7899a8cbe386107e8de549dba
Jan <hentschelj08@gmail.com>
2024-10-27 17:04:29 +0100

    added files for every important gui element

--- GUI.py
+++ GUI.py
@@ -39,7 +39,6 @@ toolbar = Frame(root, height=68)
 toolbar.pack(fill="x")
 
 
-
 # Horizontales Pained Window erstellen
 horizontally_paned_window = PanedWindow(root, orient=tk.HORIZONTAL)
 horizontally_paned_window.pack(fill="both", expand=1)
@@ -50,12 +49,9 @@ file_tree_widget = Frame(horizontally_paned_window, bg="blue")
 horizontally_paned_window.add(file_tree_widget)
 
 
-
-
 # Text-Feld für den Code Editor erstellen (mitte)
 code_editor_widget = Text(horizontally_paned_window)  
 horizontally_paned_window.add(code_editor_widget)  
-
 
 
 # Vertikales Pained Window im Horizontalen Pained Window erstellen (rechts)
@@ -63,18 +59,15 @@ vertically_pained_window = PanedWindow(horizontally_paned_window, orient=tk.VERT
 horizontally_paned_window.add(vertically_pained_window)  
 
 
-
-# Das Grid erstellen (oben rechts)
-grid_widget = Frame(vertically_pained_window, bg="green")  
-vertically_pained_window.add(grid_widget)  
-grid_label = Label(grid_widget, text="Game Grid", bg="lightblue")
-grid_label.pack(expand=True)
+# Das Virtual Environment erstellen (oben rechts)
+virtual_environment_widget = Frame(vertically_pained_window, bg="green")  
+vertically_pained_window.add(virtual_environment_widget)  
 
 
 # Terminal Textfeld erstellen (unten rechts)
-terminal_widget = Text(vertically_pained_window, bg="black", fg="white")  # Create a Text widget as a terminal
-terminal_widget.insert(tk.END, "Terminal output here...\n")
-vertically_pained_window.add(terminal_widget)  # Add the terminal to the vertical PanedWindow
+terminal_widget = Text(vertically_pained_window, bg="black")
+vertically_pained_window.add(terminal_widget)  
+
 
 
 # Alle widgets updaten, um die screen_width/screen_height zu updaten
