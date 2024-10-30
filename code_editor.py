35e4336aabd4d880689a4cafa5e2db24e6e83dcc
Jan <hentschelj08@gmail.com>
2024-10-30 20:44:11 +0100

    added scrollbars

--- code_editor.py
+++ code_editor.py
@@ -2,7 +2,21 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
-from gui import horizontally_paned_window 
+from gui import horizontally_paned_window, root
 
 # Text-Feld für den Code Editor erstellen (mitte)
-code_editor_widget = Text(horizontally_paned_window, bg="#3F3F3F", fg="white", bd=0)
+code_editor_widget = Text(horizontally_paned_window, bg="#3F3F3F", fg="white", bd=0, wrap="none")
+
+#scrollbars
+vertical_scrollbar = Scrollbar(code_editor_widget, orient="vertical", cursor="arrow")
+vertical_scrollbar.pack(side = RIGHT, fill=Y)
+vertical_scrollbar.config(command = code_editor_widget.yview)
+code_editor_widget["yscrollcommand"] = vertical_scrollbar.set
+
+
+horizontal_scrollbar = Scrollbar(code_editor_widget, orient="horizontal", cursor="arrow")
+horizontal_scrollbar.pack(side = BOTTOM, fill=X)
+horizontal_scrollbar.config(command = code_editor_widget.xview)
+code_editor_widget["xscrollcommand"] = horizontal_scrollbar.set
+
+code_editor_widget.insert(tk.END, "Code Editor")
--- terminal.py
+++ terminal.py
@@ -5,6 +5,18 @@ from tkinter import ttk
 from gui import vertically_pained_window
 
 # Terminal Textfeld erstellen (unten rechts)
-terminal_widget = Text(vertically_pained_window, bg="#3F3F3F", fg="white", bd=0)
+terminal_widget = Text(vertically_pained_window, bg="#3F3F3F", fg="white", bd=0, wrap="none")
+
+#scrollbars
+vertical_scrollbar = Scrollbar(terminal_widget, orient="vertical", cursor="arrow")
+vertical_scrollbar.pack(side = RIGHT, fill=Y)
+vertical_scrollbar.config(command = terminal_widget.yview)
+terminal_widget["yscrollcommand"] = vertical_scrollbar.set
+
+
+horizontal_scrollbar = Scrollbar(terminal_widget, orient="horizontal", cursor="arrow")
+horizontal_scrollbar.pack(side = BOTTOM, fill=X)
+horizontal_scrollbar.config(command = terminal_widget.xview)
+terminal_widget["xscrollcommand"] = horizontal_scrollbar.set
 
 terminal_widget.insert(tk.END, "Terminal")
