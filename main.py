7f5ae94bdb7329e6a077922423aa3151b88e0b3f
Jan <hentschelj08@gmail.com>
2024-11-24 12:35:06 +0100

    added python313 to the base install

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-import os
-import sys
 from gui import root
-from tests_and_debugging import debugPrints
+
 from code_execution import executeCode
-
-
 
 if __name__ == "__main__":
     root.bind("z", executeCode)
-    #debugPrints()
     root.mainloop()
 
