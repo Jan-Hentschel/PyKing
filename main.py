cd9bcec07552dab2f3caa7aabde9712699f8d8c9
Jan <hentschelj08@gmail.com>
2024-12-23 20:24:45 +0100

    added snake movement

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from tkinter import ttk
 from gui import root
 
 from code_execution import executeCode
+from virtual_environment import *
 
 if __name__ == "__main__":
     root.bind("z", executeCode)
+    root.bind("<Up>", forward_test)
+    root.bind("<Right>", turn_right_test)
+    root.bind("<Down>", eat_test)
+    root.bind("<Left>", spit_test)
     root.mainloop()
 
--- virtual_environment.py
+++ virtual_environment.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
+from terminal import terminal_widget
 from gui import vertically_pained_window, resource_path
  
 # Das Virtual Environment erstellen (oben rechts)
@@ -21,7 +22,7 @@ class GridManager:
     def create_cells(self):
         for y in range(self.grid_height):
             for x in range(self.grid_width):
-                current_cell = GridCell(x, y)
+                current_cell = GridCell(x, y, "empty")
                 current_cell.append_to_cells()
 
     def add_cells_to_grid(self):
@@ -32,14 +33,16 @@ class GridManager:
         for cell in self.cells:
             if cell.x==x and cell.y == y:
                 cell.display_image(image)
+
 class GridCell:
-    def __init__(self, x, y):
+    def __init__(self, x, y, type):
         self.x = x
         self.y = y
+        self.type = type
         self.canvas = Canvas(grid_frame, width=100, height=100, background="#3F3F3F", highlightthickness=0)
 
     def add_to_grid(self):
-        self.canvas.create_text(50, 50, text=f"x={self.x}, y={self.y}", fill="white")
+        #self.canvas.create_text(50, 50, text=f"x={self.x}, y={self.y}", fill="white")
         self.canvas.grid(row=grid_man.grid_height - self.y, column=self.x, padx=5, pady=5, sticky=N+E+S+W)
 
     def append_to_cells(self):
@@ -48,15 +51,99 @@ class GridCell:
     def display_image(self, image):
         self.canvas.create_image(18, 18, image=image, anchor=NW)
 
-grid_man = GridManager(7, 5)
+    def delete_cell(self):
+        self.canvas.delete("all")
+
+    def change_cell_type(self, new_type):
+        self.type = new_type
+
+
+
+class Snake:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        for cell in grid_man.cells:
+            if cell.x == self.x and cell.y == self.y:
+                self.cell = cell
+        self.cell.display_image(logo)
+
+    def update_cell(self):
+        for cell in grid_man.cells:
+            if cell.x == self.x and cell.y == self.y:
+                self.cell = cell
+
+    def forward(self):
+        self.delete_snake_image()
+        match self.direction:
+            case "N":
+                if self.y+1 < grid_man.grid_height:
+                    self.y +=1
+                else:
+                    terminal_widget.insert(tk.END, "Du bist gegen eine Wand gelaufen\n")                    
+            case "E":
+                if self.x+1 < grid_man.grid_width:
+                    self.x +=1
+                else:
+                    terminal_widget.insert(tk.END, "Du bist gegen eine Wand gelaufen\n")
+            case "S":
+                if self.y-1 >= 0:
+                    self.y -=1
+                else:
+                    terminal_widget.insert(tk.END, "Du bist gegen eine Wand gelaufen\n")                    
+            case "W":
+                if self.x-1 >= 0:
+                    self.x -=1
+                else:
+                    terminal_widget.insert(tk.END, "Du bist gegen eine Wand gelaufen\n")                    
+        self.update_cell()
+        self.show_snake()
+
+    def turn_right(self):
+        match self.direction:
+            case "N":
+                self.direction="E"
+            case "E":
+                self.direction="S"
+            case "S":
+                self.direction="W"
+            case "W":
+                self.direction="N"
+            
+
+
+    def eat(self):
+        terminal_widget.insert(tk.END, "ate")
+
+    def spit(self):
+        terminal_widget.insert(tk.END, "spat")
+
+    def show_snake(self):
+        self.cell.display_image(logo)
+    
+    def delete_snake_image(self):
+        self.cell.delete_cell()
+
+
+
+
+grid_man = GridManager(10, 4)
 grid_man.create_cells()
 grid_man.add_cells_to_grid()
-grid_man.display_image_at_position(logo, 1, 1)
+
+snake = Snake(0, 0, "N")
 
 
 
+def forward_test(event):
+    snake.forward()
 
+def turn_right_test(event):
+    snake.turn_right()
 
+def eat_test(event):
+    snake.eat()
 
-
-        
+def spit_test(event):
+    snake.spit()
